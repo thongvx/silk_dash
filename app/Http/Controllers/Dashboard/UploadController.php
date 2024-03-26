72230e15bff127dbf148a74873ddd3030ecefa4c
thongvuong <vuongthong388@gmail.com>
2024-03-26 23:10:58 +0700

    thuật toán chọn server

--- app/Http/Controllers/Dashboard/UploadController.php
+++ app/Http/Controllers/Dashboard/UploadController.php
@@ -5,13 +5,49 @@ namespace App\Http\Controllers\Dashboard;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class UploadController
 {
+    //Trả về giao diện upload
     public function upload(Request $request){
-        /*Todo: Code phần upload
-        */
         return view('dashboard.upload');
+    }
+
+    //Xử lý gọi vào hàm này để đẩy videos lên
+    public function uploadVideo(Request $request){
+        //Kiểm tra xem có đang phải là một tiến trình mới hay không nếu có thì mới chọn lại con server
+        //Dựa trên file băm - có thuộc tính gì đó của thằng file gốc
+        $thuocTinhFilegoc = 'abcdef';
+        if (Redis::setnx($thuocTinhFilegoc)){
+            //Todo: Thêm redis exprire cho nó sau 30p 1 tiếng
+
+
+            //Todo: Lấy danh sach các server có thể upload
+            $serverUploads = [];
+
+            //Cờ để lấy server
+            $keyToGet = Redis::get('keyToGet');
+            if (!isset($keyToGet)){
+                $keyToGet = 0;
+                Redis::set('keyToGet', $keyToGet);
+            }
+
+
+            //Gắn lại lại cờ
+            $keyToGet = $keyToGet + 1;
+            if ($keyToGet >= count($serverUploads)){
+                $keyToGet = 0;
+            }
+            Redis::set('keyToGet', $keyToGet);
+        }
+
+        $choiceServer = $serverUploads[$keyToGet];
+
+        //Todo: Xử lý upload lên con choise server được chọn
+        //Forward Request $request to server được chon
+
+
     }
 
 }
--- app/Http/Controllers/Dashboard/VideoController.php
+++ app/Http/Controllers/Dashboard/VideoController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Models\Video;
 use App\Repositories\VideoRepo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Redis;
+
 
 class VideoController
 {
@@ -21,40 +19,6 @@ class VideoController
 
     }
 
-    public function uploadVideo(Request $request){
-        //Kiểm tra xem có đang phải là một tiến trình mới hay không nếu có thì mới chọn lại con server
-        //Dựa trên file băm - có thuộc tính gì đó của thằng file gốc
-        $thuocTinhFilegoc = 'abcdef';
-        if (Redis::setnx($thuocTinhFilegoc)){
-            //Todo: Thêm redis exprire cho nó sau 30p 1 tiếng
-
-
-            //Todo: Lấy danh sach các server có thể upload
-            $serverUploads = [];
-
-            //Cờ để lấy server
-            $keyToGet = Redis::get('keyToGet');
-            if (!isset($keyToGet)){
-                $keyToGet = 0;
-                Redis::set('keyToGet', $keyToGet);
-            }
-
-
-            //Gắn lại lại cờ
-            $keyToGet = $keyToGet + 1;
-            if ($keyToGet >= count($serverUploads)){
-                $keyToGet = 0;
-            }
-            Redis::set('keyToGet', $keyToGet);
-        }
-
-        $choiceServer = $serverUploads[$keyToGet];
-
-        //Todo: Xử lý upload lên con choise server được chọn
-        //Forward Request $request to server được chon
-
-
-    }
     // Hiển thị danh sách các video của user
     public function index(Request $request)
     {
