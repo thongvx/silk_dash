cb9e095479a50b98fe63189fd414d35dd3e4cbb2
thongvuong <vuongthong388@gmail.com>
2024-03-26 21:54:10 +0700

    thuật toán chọn server

--- app/Http/Controllers/Dashboard/VideoController.php
+++ app/Http/Controllers/Dashboard/VideoController.php
@@ -7,10 +7,11 @@ use App\Repositories\VideoRepo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 
 class VideoController
 {
-    protected VideoRepo $videoRepo;
+    protected  $videoRepo;
 
     public function __construct(VideoRepo $videoRepo){
         $this->videoRepo = $videoRepo;
@@ -18,6 +19,29 @@ class VideoController
 
     public function video(Request $request){
 
+    }
+
+    public function uploadVideo(Request $request){
+        //Todo: Lấy danh sach các server có thể upload
+        $serverUploads = [];
+
+        //Cờ để lấy server
+        $keyToGet = Redis::get('keyToGet');
+        if (!isset($keyToGet)){
+            $keyToGet = 0;
+            Redis::set('keyToGet', $keyToGet);
+        }
+
+        $choiceServer = $serverUploads[$keyToGet];
+
+        //Todo: Xử lý upload lên con choise server được chọn
+
+        //Gắn lại lại cờ
+        $keyToGet = $keyToGet + 1;
+        if ($keyToGet >= count($serverUploads)){
+            $keyToGet = 0;
+        }
+        Redis::set('keyToGet', $keyToGet);
     }
     // Hiển thị danh sách các video của user
     public function index(Request $request)
--- resources/views/dashboard/videos/index.blade.php
+++ resources/views/dashboard/videos/index.blade.php
@@ -1,5 +1,7 @@
 @extends('dashboard.layouts.app')
 
 @section('content')
-
+@foreach($videos as $video)
+    {{$video->name}}
+@endforeach
 @endsection
