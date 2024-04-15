6e82df18deb5df7fbdf4785c06d3c770225ff972
duong1811 <diemduong171@gmail.com>
2024-04-15 09:56:19 +0700

    update webupload

--- resources/views/upload/webupload.blade.php
+++ resources/views/upload/webupload.blade.php
@@ -4,14 +4,13 @@
             You can upload multiple video files per a session with total sizes up to 100 GB
         </div>
         <hr class="h-px my-6 bg-transparent bg-gradient-to-r from-transparent via-white to-transparent border-none" />
-        <form class='lg:mx-32 from-current' method="POST" id="form-upload-file"
+        <form class='lg:mx-32 from-current' method="POST" id="form-upload-file" action="https://up.sptvp.com/upload"
               enctype="multipart/form-data">
             @csrf
             <label htmlfor="file" class="rounded-xl py-10 bg-slate-900 flex justify-center flex-col h-full w-full relative ">
                 <span class='font-semibold text-green-400'>Select Video files to upload</span>
                 <p class="pl-1">or drag and drop</p>
                 <input id="file"  name="file" accept="video/*" type="file"
-                       data-url="https://up.sptvp.com/upload"
                        multiple class="opacity-0 absolute cursor-pointer z-20 h-full w-full top-0 left-0" />
             </label>
             <input class="hidden" type="text" id="userID" name="userID" value="1">
