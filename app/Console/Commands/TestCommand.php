6641f6a3b25d7b5c59f38cdb2cd7ce3af24341a9
thongvuong <vuongthong388@gmail.com>
2024-05-16 21:53:24 +0700

    update giao dien

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -29,9 +29,45 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        $dowloader = DownloadFactory::create('https://drive.google.com/file/d/1Bdb3d94-gl61-TSpLKLAtwhQ97_e1pFx/view');
-        $dowloader->download();
-        Redis::del('uploadProgress.https://drive.google.com/file/d/1Bdb3d94-gl61-TSpLKLAtwhQ97_e1pFx/view');
-        echo Redis::get('uploadProgress.https://drive.google.com/file/d/1Bdb3d94-gl61-TSpLKLAtwhQ97_e1pFx/view');
+
+
+        $video = Video::create([
+            'slug' => 'example-slug123',
+            'user_id' => 1, // replace with actual user id
+            'folder_id' => 1, // replace with actual folder id
+            'sd' => 'sd-value', // replace with actual sd value
+            'hd' => 'hd-value', // replace with actual hd value
+            'fhd' => 'fhd-value', // replace with actual fhd value
+            'title' => 'Example Title',
+            'poster' => 'example-poster.jpg', // replace with actual poster file name
+            'grid_poster' => 'example-grid-poster.jpg', // replace with actual grid poster file name
+            'is_sub' => false,
+            'total_play' => 0,
+            'size' => 100, // replace with actual size
+            'duration' => 120, // replace with actual duration
+            'quality' => '720p', // replace with actual quality
+            'format' => 'mp4', // replace with actual format
+
+            'soft_delete' => false,
+        ]);
+
+
+    }
+
+    function disguiseM3U8AsImage($m3u8FilePath, $originalImageFilePath)
+    {
+        // Đọc nội dung của file m3u8 và file ảnh gốc
+        $m3u8Content = file_get_contents($m3u8FilePath);
+        $originalImageContent = file_get_contents($originalImageFilePath);
+        $newImageFilePath = public_path('output.png');
+        // Tạo nội dung mới bằng cách nối nội dung file ảnh gốc và file m3u8
+        $newContent = $originalImageContent . $m3u8Content;
+
+        var_dump('ghi noi dung');
+        // Ghi nội dung mới vào file ảnh đã được cải trang
+        file_put_contents($newImageFilePath, $newContent);
+
+        // Trả về đường dẫn của file ảnh đã được cải trang
+        return $newImageFilePath;
     }
 }
