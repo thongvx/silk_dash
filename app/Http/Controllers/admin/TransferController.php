7c87a3ee71c11a11534fcb376849f236f9168515
ngocphuongsv1 <ngocphuongsv1@gmail.com>
2024-05-20 19:56:03 +0700

    update transfer 1

---
+++ app/Http/Controllers/admin/TransferController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Transfer;
+use App\Models\SvEncoder;
+use Illuminate\Support\Facades\Redis;
+
+class TransferController extends Controller
+{
+    //-------------------------------start transfer task-------------------------------------------
+    public function startTransferTask()
+    {
+        $data = Transfer::where('status', 0)->orderBy('priority', 'desc')->first();
+        if(!empty($data)){
+            //select server transfer
+            $domain = SvEncoder::where('active', 1)->where('transfer', '<', 10)->inRandomOrder()->value('name');
+            $data->sv_transfer = $domain;
+            $data->status = 1;
+            $data->save();
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://'.$domain.'.streamsilk.com/insertDataTransfer?user_id='.$data->user_id.'&folder_id='.$data->folder_id.'&slug='.$data->slug.'&url='.$data->url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            return true;
+        }
+    }
+    //-------------------------------update link transfer------------------------------------------
+    public function updateLinkTransfer(Request $request)
+    {
+        $user_id = $request->user_id;
+        $slug = $request->slug;
+        $progress = $request->progress;
+        $size_download = $request->size_download;
+        $size = $request->size;
+        Redis::get('transfer'.$user_id.'-'.$slug, json_encode([
+            'progress' => $progress,
+            'size_download' => $size_download,
+            'size' => $size,
+        ]);
+    }
+    //=============================================================================================
+}
---
+++ database/migrations/2024_05_20_082052_transfer.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transfer', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->nullable();
+            $table->string('slug', 20)->nullable();
+            $table->string('title', 500)->nullable();
+            $table->integer('priority')->nullable();
+            $table->string('url', 500)->nullable();
+            $table->integer('status')->nullable();
+            $table->string('sv_transfer', 5)->nullable();
+            $table->integer('folder_id')->nullable();
+            $table->integer('progress')->nullable();
+            $table->bigInteger('size_download')->nullable();
+            $table->bigInteger('size')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
