367485258134ccffa2311e1ac70b6e6c06553e96
nnphuong <ngocphuongsv1@gmail.com>
2024-06-01 20:27:57 +0700

    update upload video

--- app/Http/Controllers/Dashboard/UploadController.php
+++ app/Http/Controllers/Dashboard/UploadController.php
@@ -6,9 +6,6 @@ namespace App\Http\Controllers\Dashboard;
 use App\Repositories\FolderRepo;
 use Illuminate\Support\Facades\Auth;
 use App\Factories\DownloadFactory;
-use App\Models\User;
-use App\Models\Video;
-use App\Models\EncoderTask;
 use App\Models\Transfer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
@@ -50,90 +47,7 @@ class UploadController
         }
 
     }
-    //Xử lý gọi vào hàm này để đẩy video lên
-    public function uploadVideo(Request $request){
-        $videoInfo = $request->all();
-        $userId = $videoInfo['userId'];
-        $user = User::find($userId);
-        //Update user
-        $videoSize = $videoInfo['size'];
-        $user->increment('video');
-        $user->increment('storage', $videoSize);
-        $user->last_upload = time();
-        $user->save();
-        //check video
-        $check_duplicate = md5($videoInfo['duration'].$videoSize.$videoInfo['quality']);
-        $video = Video::where('check_duplicate', $check_duplicate)->first();
-        $videoData = [
-            'slug' => $videoInfo['slug'],
-            'user_id' => $userId,
-            'folder_id' => $videoInfo['folder'],
-            'title' => $videoInfo['title'],
-            'poster' => '0',
-            'grid_poster_3' => '0',
-            'is_sub' => 0,
-            'total_play' => 0,
-            'size' => $videoSize,
-            'duration' => $videoInfo['duration'],
-            'quality' => $videoInfo['quality'],
-            'format' => $videoInfo['format'],
-            'origin' => 0,
-            'soft_delete' => 0,
-            'stream' => 0,
-            'grid_poster_5' => '0',
-        ];
-        if($video){
-            //create video exist
-            $videoData['middle_slug'] = $video->middle_slug;
-            $videoData['sd'] = $video->sd;
-            $videoData['hd'] = $video->hd;
-            $videoData['fhd'] = $video->fhd;
-            $videoData['check_duplicate'] = 0;
-        }else{
-            $encoderPriority = $user->encoder_priority;
-            $videoData['middle_slug'] = $videoInfo['slug'];
-            $videoData['sd'] = '0';
-            $videoData['hd'] = '0';
-            $videoData['fhd'] = '0';
-            $videoData['sv_upload'] = $videoInfo['sv'];
-            $videoData['check_duplicate'] = $check_duplicate;
-            //create encoder task 480
-            $encoderTask480 = new EncoderTask();
-            $encoderTask480->insertEncoderTask($videoData, $encoderPriority, 480);
-            $encoderTask480->save();
-            //create encoder task 720
-            if($videoInfo['quality'] > 480){
-                $encoderTask720 = new EncoderTask();
-                $encoderTask720->insertEncoderTask($videoData, $encoderPriority, 720);
-                $encoderTask720->save();
-            }
-            //create encoder task 1080
-            if($videoInfo['quality'] > 720){
-                $encoderTask1080 = new EncoderTask();
-                $encoderTask1080->insertEncoderTask($videoData, $encoderPriority, 1080);
-                $encoderTask1080->save();
-            }
-        }
-        $video = Video::create($videoData);
-        //check transfer
-        $dataTransfer = Transfer::where('user_id', $userId)->where('slug', $videoInfo['slug'])->first();
-        if($dataTransfer){
-            $dataTransfer->status = 2;
-            $dataTransfer->progress = 100;
-            $dataTransfer->size_download = $videoSize;
-            $dataTransfer->size = $videoSize;
-            $dataTransfer->save();
-            Redis::set('transfer'.$videoInfo['userId'].'-'.$videoInfo['slug'], json_encode([
-                'slug' => $videoInfo['slug'],
-                'url' => $dataTransfer->url,
-                'status' => 2,
-                'progress' => 100,
-                'size_download' => $videoSize,
-                'size' => $videoSize,
-            ]));
-        }
-        return response()->json(['status' => 'success', 'message' => 'Upload success']);
-    }
+
     public function remoteUploadDirect(Request $request)
     {
         $url = $request->input('url');
