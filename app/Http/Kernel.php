604038e4e581d409e24ed5f7b33e764918021b3f
thongvuong <vuongthong388@gmail.com>
2024-06-08 15:42:34 +0700

    update code

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ShareMinimenu::class,
         ],
 
         'api' => [
