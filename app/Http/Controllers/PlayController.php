f0d75dc9de44fd8a78c3b5c883f2a1726c207688
nnphuong <ngocphuongsv1@gmail.com>
2024-06-08 21:37:20 +0700

    update play

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -13,9 +13,10 @@ class PlayController
 {
     public function play($slug)
     {
-
         $data = Video::where('slug', $slug)->first();
         if(!empty($data) && $data->soft_delete == 0){
+            $user_id = $data->user_id;
+            $title = $data->title;
             if($data->origin == 0){
                 //play origin
                 $svUpload = EncoderTask::where('slug', $slug)->where('quality', 480)->value('sv_upload');
@@ -23,7 +24,12 @@ class PlayController
                 return view('play', ['urlPlay' => $urlPlay]);
             }
             else{
+                if($data->check_duplicate == 0)
+                    $data = Video::where('slug', $data->middle_slug)->first();
                 //check stream
+                if($data->pathStream == 0)
+                    $data->pathStream = $this->selectPathStream($data->sd, $data->hd, $data->fhd);
+
                 if($data->stream == 0){
                     $svStream = $this->selectSvStream();
                     Queue::push(new CreateHlsJob($data->middle_slug, $svStream, $data->pathStream, $data->sd, $data->hd, $data->fhd));
@@ -34,7 +40,7 @@ class PlayController
                     $Stream = $data->stream;
                     $arrStream = explode('-', $Stream);
                     $svStream = $this->checkConnectSvStream($arrStream);
-                    if(!empty($svStream)){
+                    if(empty($svStream)){
                         $svStream = $this->selectSvStream();
                         Queue::push(new CreateHlsJob($data->middle_slug, $svStream, $data->pathStream, $data->sd, $data->hd, $data->fhd));
                         $data->stream = $data->stream .'-'. $svStream;
@@ -65,6 +71,18 @@ class PlayController
             ->orderBy('out_speed', 'asc')
             ->value('name');
         return $svStream;
+    }
+    //-----------------------------select path stream-----------------------------------------------------
+    function selectPathStream($sd, $hd, $fhd)
+    {
+        $path = '0';
+        if($sd != 0)
+            $path = $sd;
+        elseif ($hd != 0)
+            $path = $hd;
+        elseif ($fhd != 0)
+            $path = $fhd;
+        return $path;
     }
     //====================================================================================================
 }
