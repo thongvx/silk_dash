2744ec0cd5d1e6f7cb77892462f20a57d7169c6b
thongvuong <vuongthong388@gmail.com>
2024-06-20 08:36:57 +0700

    update proxy

--- app/Http/Controllers/Play/VideoViewController.php
+++ app/Http/Controllers/Play/VideoViewController.php
@@ -10,13 +10,13 @@ class VideoViewController
     public function updateView($slug, Request $request)
     {
         $keyPerIp = "user_views:{$request->ip()}";
-        var_dump($keyPerIp); die;
+
         $views = Redis::get($keyPerIp) ?: 0;
 
-        var_dump($keyPerIp);
+
         //1 ngày 1 ip chỉ được tính 2 view thôi
         if ($views < 2) {
-            var_dump('views: '. $views);
+
             $views++;
             Redis::setex($keyPerIp, 24 * 60 * 60, $views);
 
@@ -26,9 +26,6 @@ class VideoViewController
             //Tăng view cho video
             $key = "video_views:{$slug}:{$country}";
             Redis::incr($key);
-            var_dump('da tang view');
-        }else{
-            var_dump('da vuot qua so luong view');
         }
     }
 
