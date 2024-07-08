4676ad29800610fedce66e5dfec4866c51d09659
duong1811 <diemduong171@gmail.com>
2024-07-08 16:55:55 +0700

    update play

--- resources/views/dashboard/setting/playersetting.blade.php
+++ resources/views/dashboard/setting/playersetting.blade.php
@@ -88,20 +88,6 @@
                             Infinite Loop
                         </label>
                     </div>
-                    <div class="items-center mt-3 flex">
-                        <input type="checkbox" id="disable_adblock" class="rounded-xl duration-200 ease-in-out after:rounded-full after:shadow-2xl
-                                                         after:duration-200 checked:after:translate-x-5 h-5 relative
-                                                         float-left mt-1 w-10 cursor-pointer appearance-none border
-                                                         border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain
-                                                         bg-left bg-no-repeat align-top transition-all after:absolute
-                                                         after:top-px after:h-4 after:w-4 after:translate-x-0.5 after:bg-white
-                                                         after:content-[''] checked:border-[#009FB2] checked:bg-[#009FB2]
-                                                         checked:bg-none checked:bg-right"
-                               value="{{$playerSettings->disable_adblock}}"   name="disable_adblock" {{$playerSettings->disable_adblock == 1 ? 'checked' : ''}}>
-                        <label for="disable_adblock" class="ml-3">
-                            Force to disable adblocker
-                        </label>
-                    </div>
                     <div class="items-center mt-3 w-full">
                         <div class="mt-2 md:flex items-center">
                             <label for="block" class="w-60">
