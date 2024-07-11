4c637b899540e4f581390909d3c230835f123abb
nnphuong <ngocphuongsv1@gmail.com>
2024-07-11 10:21:39 +0700

    update phuong

--- app/Http/Controllers/VideoViewController.php
+++ app/Http/Controllers/VideoViewController.php
@@ -19,38 +19,36 @@ class VideoViewController
         Redis::incr("ahihidongok");
         $origin = $request->headers->get('Origin');
         $referer = $request->headers->get('Referer');
-//
         $keyPerIp = "user_views:{$request->ip()}";
         $views = Redis::get($keyPerIp) ?: 0;
-        return $views;
-//
-//        //1 ngày 1 ip chỉ được tính 2 view thôi
-//        if ($views < 2 ) {
-//
-//            $views++;
-//            Redis::setex($keyPerIp, 24 * 60 * 60, $views);
-//
-//            //Lấy country truy cập hệ thống ở đây
-//            $country = $request->header('CF-IPCountry');
-//
-//
-//            //Tăng view cho video
-//
-//            $video = $this->videoRepo->findVideoBySlug($slug);
-//            if (!$video){
-//                return response()->json(['status' => 'fail']);
-//            }
-//            $watchingUserKey ="watching_users:{$video->user_id}";
-//            $totalViewKey = "total:{$video->user_id}:{$country}";
-//            $keyWithCountry = "country_video_views:{$video->id}:{$video->user_id}:{$country}";
-//            $key = "video_views:{$video->id}:{$video->user_id}";
-//            Redis::incr($key);
-//            Redis::incr($keyWithCountry);
-//            Redis::incr($totalViewKey);
-//            Redis::incr($watchingUserKey);
-//            return response()->json(['status' => 'success']);
-//        }
-//        return response()->json(['status' => 'fail']);
+
+        //1 ngày 1 ip chỉ được tính 2 view thôi
+        if ($views < 2 ) {
+
+            $views++;
+            Redis::setex($keyPerIp, 24 * 60 * 60, $views);
+
+            //Lấy country truy cập hệ thống ở đây
+            $country = $request->header('CF-IPCountry');
+
+
+            //Tăng view cho video
+
+            $video = $this->videoRepo->findVideoBySlug($slug);
+            if (!$video){
+                return response()->json(['status' => 'fail']);
+            }
+            $watchingUserKey ="watching_users:{$video->user_id}";
+            $totalViewKey = "total:{$video->user_id}:{$country}";
+            $keyWithCountry = "country_video_views:{$video->id}:{$video->user_id}:{$country}";
+            $key = "video_views:{$video->id}:{$video->user_id}";
+            Redis::incr($key);
+            Redis::incr($keyWithCountry);
+            Redis::incr($totalViewKey);
+            Redis::incr($watchingUserKey);
+            return response()->json(['status' => 'success']);
+        }
+        return response()->json(['status' => 'fail']);
     }
 
 }
