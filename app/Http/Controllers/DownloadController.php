a63a22ff87190dfa4df8713424fdc90ecf403d2d
nnphuong <ngocphuongsv1@gmail.com>
2024-07-19 01:00:49 +0700

    update phuong

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\CreateDownload;
 use App\Repositories\AccountRepo;
 use App\Repositories\PlayerSettingsRepo;
 use App\Repositories\VideoRepo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Queue;
+
 class DownloadController extends Controller
 {
     protected $videoRepo, $accountRepo, $playerSettingsRepo;
@@ -26,5 +29,26 @@ class DownloadController extends Controller
             return response()->view('errors.404', [], 404);
         }
         return view('download', $data);
+    }
+    public function addDownloadVideo(Request $request)
+    {
+        $slug = $request->slug;
+        $quality = $request->quality;
+        $path = $request->path;
+
+        Queue::push(new CreateDownload($slug, 'd01', $quality, $path));
+
+        return 'success';
+    }
+    function creatTokenDownload(){
+        $exp = time()+3600;
+
+        $string_hash = $exp.' Silk@2024';
+        $md5_hash = md5($string_hash, true);
+        $base64_hash = base64_encode($md5_hash);
+        $base64_hash = strtr($base64_hash, '+/', '-_');
+        $base64_hash = rtrim($base64_hash, '=');
+
+        return '?token='.$base64_hash.'&expires='.$exp;
     }
 }
