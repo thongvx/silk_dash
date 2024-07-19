54ac183816963ba710a299ee97faed57780868cc
nnphuong <ngocphuongsv1@gmail.com>
2024-07-19 10:01:53 +0700

    phuong

---
+++ database/migrations/2024_07_19_025454_create_sv_download_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sv_download', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 10)->nullable();
+            $table->string('ip', 20)->nullable();
+            $table->string('pass', 50)->nullable();
+            $table->integer('port')->nullable();
+            $table->integer('number_video')->nullable();
+            $table->float('cpu')->nullable();
+            $table->float('space')->nullable();
+            $table->float('used_space')->nullable();
+            $table->float('percent_space')->nullable();
+            $table->integer('max_speed')->nullable();
+            $table->float('in_speed')->nullable();
+            $table->float('out_speed')->nullable();
+            $table->string('provider', 20)->nullable();
+            $table->boolean('in_data')->default(true);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sv_download');
+    }
+};
