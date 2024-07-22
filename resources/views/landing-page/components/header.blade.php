ce381c2a4cbfbabb7deff08e7b1b8e47c2f4a1b4
nnphuong <ngocphuongsv1@gmail.com>
2024-07-22 20:32:31 +0700

    phuong

--- resources/views/landing-page/components/header.blade.php
+++ resources/views/landing-page/components/header.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="clckd" content="a30efa94d69745bec06f7a296338c8ef" />
     <link rel="icon" type="image/png" href="{{ asset('image/logo/logo4.webp') }}" />
     <title>Stream Silk</title>
     @vite('resources/css/app.css')
