2bf3059e9ebca38707705d4588f9381c3b4150c8
duong1811 <diemduong171@gmail.com>
2024-08-05 16:49:13 +0700

    update player

--- resources/js/support/ticket.js
+++ resources/js/support/ticket.js
@@ -1,6 +1,6 @@
 import {add_notification, updateOriginalFormState} from "../main.js";
 
-$(document).on('submit', '#ticket-form', function(e) {
+$(document).on('submit', '#ticket-form', function (e) {
     e.preventDefault();
     button = $(this).find('[btn-save]');
     let formData = new FormData(this);
@@ -10,55 +10,56 @@ $(document).on('submit', '#ticket-form', function(e) {
         data: formData,
         processData: false,
         contentType: false,
-        success: function(response) {
+        success: function (response) {
             const message = 'Created successfully!';
-            add_notification('success',message, button);
+            add_notification('success', message, button);
             updateOriginalFormState();
             $(form).find('input[type="file"]').val('');
         },
-        error: function(response) {
+        error: function (response) {
             console.log(response);
         }
     });
 })
 
-$(document).on('click', '[btn-get-token]', function() {
+$(document).on('click', '[btn-get-token]', function () {
 
     $.ajax({
         type: 'GET',
         url: '/regenerateToken',
-        success: function(response) {
+        success: function (response) {
             $('#token').html(`<div class="relative flex">
                                     <h5>API Token: <span class="text-clipboard">${response.token}</span></h5>
                                     <i class="material-symbols-outlined ml-4 cursor-pointer hover:text-blue-500 text-md" clipboard-copy>content_copy</i>
                                 </div>
                                 <button class="rounded-lg ml-3 hover:text-[#009fb2] cursor-pointer" btn-get-token>
                                     <i class="material-symbols-outlined opacity-1 text-2xl">autorenew</i>
-                                </button>` );
+                                </button>`);
             $('#token').addClass('justify-between')
             const div_notification = `<div class="justify-center w-full text-green-400 mt-2 items-center flex" id="noti-warning">
                                                 <i class="material-symbols-outlined mr-2">done</i>
                                                 Token regenerated successfully!
                                             </div>`
             $('#token').after(div_notification);
-            setInterval(function() {
+            setInterval(function () {
                 $('#noti-warning').remove();
             }, 2000);
         },
-        error: function(response) {
+        error: function (response) {
             const div_notification = `<div class="justify-center w-full text-red-400 mt-2 items-center flex" id="noti-warning">
                                         <i class="material-symbols-outlined mr-2">error</i>
                                         Token regenerated failed!
                                     </div>`
             $('#token').after(div_notification);
-            setInterval(function() {
+            setInterval(function () {
                 $('#noti-warning').remove();
             }, 2000);
         }
     });
 })
 
-$(document).on('click', '#list-menu-api li', function() {
+$(document).on('click', '#list-menu-api li', function () {
     $('#list-menu-api li').removeClass('bg-[#009fb2]');
     this.classList.add('bg-[#009fb2]')
 })
+
