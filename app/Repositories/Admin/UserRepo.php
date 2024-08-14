d7c901afd410c4c7f8ea24b60f54aac5f7c886e9
duong1811 <diemduong171@gmail.com>
2024-08-14 16:41:54 +0700

    update dashboard admin

--- app/Repositories/Admin/UserRepo.php
+++ app/Repositories/Admin/UserRepo.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\Admin;
 
 use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Prettus\Repository\Eloquent\BaseRepository;
 use App\Models\File;
 use Illuminate\Support\Facades\Redis;
@@ -17,44 +18,59 @@ class UserRepo
 
     public function getAllUsers($tab, $column , $direction, $limit, $columns)
     {
-        $cache_keys = 'all_users' . $tab . $column . $direction . $limit;
-        $user = Redis::get($cache_keys);
-        if(isset($user)){
-            return unserialize($user);
+        $cache_keys = 'all_users';
+        $all_users = Redis::get($cache_keys);
+        if(isset($all_users)){
+            $all_users = unserialize($all_users);
+        }else{
+            $all_users = User::all()->whereDoesntHave('roles', function ($query) {
+                $query->where('name', 'admin');
+            });
+            Redis::setex($cache_keys, 259200, serialize($all_users));
         }
-        $query = User::query()
-                ->whereDoesntHave('roles', function ($query) {
-                        $query->where('name', 'admin');
-                    });
-        $column == 'created_at' ? $column1 = 'id' : $column1 = $column;
-        switch ($tab){
+
+        $column1 = $column == 'created_at' ? 'id' : $column;
+
+        switch ($tab) {
             case 'unverified':
-                $query->whereIn('active', [0, null]);
+                $all_users = $all_users->whereIn('active', [0, null]);
                 break;
             case 'delete':
-                $query->where('active', 19);
+                $all_users = $all_users->where('active', 19);
                 break;
             case 'premium':
-                $query->whereIn('premium', [1,2,3]);
+                $all_users = $all_users->whereIn('premium', [1, 2, 3]);
                 break;
             case 'free':
-                $query->where('active', [1,2])
-                        ->where('premium', 0);
+                $all_users = $all_users->whereIn('active', [1, 2])
+                    ->where('premium', 0);
                 break;
             default:
                 break;
         }
 
-        $users = $query->orderBy($column1, $direction)
-                        ->paginate($limit);
-        Redis::setex($cache_keys, 259200, serialize($users));
+        $sortedData = $all_users->sortBy($column1, SORT_REGULAR, $direction === 'desc');
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $currentPageItems = $sortedData->slice(($currentPage - 1) * $limit, $limit)->values();
+        $users = new LengthAwarePaginator($currentPageItems, $sortedData->count(), $limit, $currentPage, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
         return $users;
     }
 
     public function getUserById($id)
     {
-        $users = $this->getAllUsers('all', 'created_at', 'desc', 100, ['*']);
-        $user = $users->find($id);
+        $cache_keys = 'all_users';
+        $all_users = Redis::get($cache_keys);
+        if(isset($all_users)){
+            $all_users = unserialize($all_users);
+        }else{
+            $all_users = User::all()->whereDoesntHave('roles', function ($query) {
+                $query->where('name', 'admin');
+            });
+            Redis::setex($cache_keys, 259200, serialize($all_users));
+        }
+        $user = $all_users->where('id', $id)->first();
         return $user;
     }
 
