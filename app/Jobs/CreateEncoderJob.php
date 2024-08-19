16db838e260bfc9d9c230f425051c1143c7f9611
nnphuong <ngocphuongsv1@gmail.com>
2024-08-19 19:04:47 +0700

    phuong

--- app/Jobs/CreateEncoderJob.php
+++ app/Jobs/CreateEncoderJob.php
@@ -40,7 +40,7 @@ class CreateEncoderJob implements ShouldQueue
             CURLOPT_TIMEOUT => 0,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_CUSTOMREQUEST => 'GET',
         ));
         $response = curl_exec($curl);
         curl_close($curl);
