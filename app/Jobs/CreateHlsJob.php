07a3c5b7e017b31ba3719ea79b6166593f8db5fa
nnphuong <ngocphuongsv1@gmail.com>
2024-08-21 00:36:53 +0700

    phuong

--- app/Jobs/CreateHlsJob.php
+++ app/Jobs/CreateHlsJob.php
@@ -33,7 +33,7 @@ class CreateHlsJob implements ShouldQueue
     {
         $curl = curl_init();
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://'.$this->sv.'./insertData?slug='.$this->slug.'&path='.$this->path.'&sto480='.$this->sto480.'&sto720='.$this->sto720.'&sto1080='.$this->sto1080,
+            CURLOPT_URL => 'https://'.$this->sv.'/insertData?slug='.$this->slug.'&path='.$this->path.'&sto480='.$this->sto480.'&sto720='.$this->sto720.'&sto1080='.$this->sto1080,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -41,6 +41,9 @@ class CreateHlsJob implements ShouldQueue
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_HTTPHEADER => array(
+                'Cookie: XSRF-TOKEN=eyJpdiI6InlzTTRnTFdCZitPYk9TSFAzK2llQXc9PSIsInZhbHVlIjoiRXRsTmRnU2tVbGg3cVBXQktCMXdOM2I0RWg2b0FmcThtVllKVW15c2x6QWNkd21wWk92NG9YV2RSMXhDRlo4U01uN2FvQXQzMmtTUmxwRDduU3ZzY2pKVGNmTWgvUVZoNFFGVlpHbWJqT29xZXMzMUtYOVpwK0xBWmZpeWNjOG8iLCJtYWMiOiJhYmYzNjI2YzJmMzk4ZjMzOWE2MWE2ZTU0NGRiMWNkZDdkMDZjMThiZWI4NTQ1MTUzOTJmMTY5NzE3YjAwYmQzIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IitJUm42UHZ6QVNaNHp0Zk1DUjhHZHc9PSIsInZhbHVlIjoiZlVpTk9JWTZTVTJUWVN0S1VlUnRNMnpMRTJ6bjN6QnZHTVBCNk1RTVZSdklLUTh0YjdIWm45bWVLalRSeDdKOFE2MmY5UzIwb0tzZUlMeHNOQkhTUDlLK3hkUXMwMndiRHJ2aktMejYvTEJHVTZUVlF0WHMxallIREdBWGpHdjkiLCJtYWMiOiIwMDQ4NjNhZGVkNWQ4MDlmNjNiYjFlNjgyM2I1MTBmYTRkNTVkNDY4Zjc1OTg1N2ZiNzhjNGVkNjMwZTE4MzU5IiwidGFnIjoiIn0%3D'
+            ),
         ));
         $response = curl_exec($curl);
         curl_close($curl);
