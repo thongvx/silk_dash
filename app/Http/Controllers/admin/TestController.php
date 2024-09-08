9160764a8bfda2748283b4c56b9df3445bcf7337
nnphuong <ngocphuongsv1@gmail.com>
2024-09-09 00:05:56 +0700

    phuong

--- app/Http/Controllers/admin/TestController.php
+++ app/Http/Controllers/admin/TestController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Video;
+use App\Models\EncoderTask;
 
 class TestController extends Controller
 {
     //------------------------------------get all video ------------------------------------
     function getAllVideo($user_id)
     {
-        $data = Video::where('user_id', $user_id)->where('id', '>', 45326)->select('slug', 'sd', 'hd', 'fhd')->get();
-        return response()->json($data);
+        $dataUpdate['status'] = 2;
+        $data = EncoderTask::where('status', 3)->update($dataUpdate);
+        echo 'ok';
     }
     //=========================================================================================================
 }
