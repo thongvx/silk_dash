fa1bddb575915db1d987f81d85ebc1d5735d87c4
duong1811 <diemduong171@gmail.com>
2024-09-13 10:22:27 +0700

    update encoder

--- app/Http/Controllers/admin/ManageTaskController.php
+++ app/Http/Controllers/admin/ManageTaskController.php
@@ -53,9 +53,11 @@ class ManageTaskController extends Controller
         $encoder = DB::table('encoder_task')->where('id', $encoderId)->first();
         // Check if the encoder exists
         if ($encoder) {
-            DB::table('sv_encoders')
-                ->where('name', $encoder->sv_encoder)
-                ->decrement('encoder');
+            if($encoder->status != 19){
+                DB::table('sv_encoders')
+                    ->where('name', $encoder->sv_encoder)
+                    ->decrement('encoder');
+            }
             // Set the status and storage values to 0
             DB::table('encoder_task')->where('id', $encoderId)->update([
                 'status' => 0,
