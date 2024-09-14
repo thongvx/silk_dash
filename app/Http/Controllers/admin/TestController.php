7c09138d47f95d96305b8af166041f2abe2dc085
nnphuong <ngocphuongsv1@gmail.com>
2024-09-15 00:23:23 +0700

    phuong

--- app/Http/Controllers/admin/TestController.php
+++ app/Http/Controllers/admin/TestController.php
@@ -13,15 +13,8 @@ class TestController extends Controller
     //------------------------------------get all video ------------------------------------
     function test1()
     {
-        $svStreamKeys = Redis::smembers('sv_streams');
-        shuffle($svStreamKeys);
-        foreach ($svStreamKeys as $svStreamKey) {
-            $svStream = Redis::hgetall($svStreamKey);
-            if ($svStream['active'] == 1 && $svStream['out_speed'] < 900 && $svStream['cpu'] < 10 && $svStream['percent_space'] < 0.95) {
-                return $svStream['domain'];
-            }
-        }
-        return null;
+        $data = EncoderTask::where('sv_uploads', 'e08')->get();
+        return response()->json($data);
     }
 
     //=========================================================================================================
