b5a960685395cb20bf7c4c94d9087e2974cc5b50
nnphuong <ngocphuongsv1@gmail.com>
2024-09-14 13:26:54 +0700

    phuong

--- app/Http/Controllers/admin/TestController.php
+++ app/Http/Controllers/admin/TestController.php
@@ -12,19 +12,14 @@ class TestController extends Controller
     //------------------------------------get all video ------------------------------------
     function test1()
     {
-        $arrStream = 'ss03';
-        $svStreamKeys = Redis::smembers('sv_streams');
+        $arrStream = 'sv_streams:ss03';
 
-        foreach ($svStreamKeys as $svStreamKey) {
-            $svStreamName = str_replace('sv_streams:', '', $svStreamKey);
-
-            if (in_array($svStreamName, $arrStream)) {
-                $svStream = Redis::hgetall($svStreamKey);
+                $svStream = Redis::hgetall($arrStream);
 
                 if ($svStream['out_speed'] < 900 && $svStream['active'] == 1) {
                     return $svStream['domain'];
                 }
-            }
+
             return 'no';
         }
     }
