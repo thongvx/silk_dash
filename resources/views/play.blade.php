399c06f1306574cb3afcb1f62fb8363a0c9592d9
duong1811 <diemduong171@gmail.com>
2024-09-14 14:00:14 +0700

    update play

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -94,7 +94,7 @@ $jsCode = <<<JS
             aspectratio: "16:9",
             jwplayer8quality: true,
             controls: true,
-            preload: preload,
+            preload: auto,
             width: '100%',
             height: '100%',
             skin: {active: " $player_setting->premium_color ",},
