ed2c2cd02876f22eae97a4874e2f1505b1891db4
nnphuong <ngocphuongsv1@gmail.com>
2024-09-14 13:37:44 +0700

    phuong

--- app/Http/Controllers/admin/TestController.php
+++ app/Http/Controllers/admin/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Services\ServerStream\SvStreamService;
 use Illuminate\Http\Request;
 use App\Models\EncoderTask;
 use Illuminate\Support\Facades\Redis;
@@ -12,16 +13,8 @@ class TestController extends Controller
     //------------------------------------get all video ------------------------------------
     function test1()
     {
-        $arrStream = 'ss03-ss01';
-        $arrStream = explode('-', $arrStream);
-        foreach ($arrStream as $valueStream){
-            $svStream = Redis::hgetall('sv_streams:'.$valueStream);
-            if ($svStream['out_speed'] < 900 && $svStream['active'] == 1) {
-                return $svStream['domain'];
-            }
-
-        }
-        return 'no';
+        $svStream = SvStreamService::selectSvStream();
+        echo $svStream;
 
     }
 
