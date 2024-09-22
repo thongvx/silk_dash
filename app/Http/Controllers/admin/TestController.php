8dbf754ec2c26fc9526e45aaac46a9924395329d
nnphuong <ngocphuongsv1@gmail.com>
2024-09-22 10:02:15 +0700

    phuong

--- app/Http/Controllers/admin/TestController.php
+++ app/Http/Controllers/admin/TestController.php
@@ -13,10 +13,9 @@ class TestController extends Controller
     //------------------------------------get all video ------------------------------------
     function test1()
     {
-        $data = EncoderTask::where('status', 0)->pluck('slug');
-        foreach ($data as $slug){
-            Redis::set('encoserTask:'.$slug, 1);
-        }
+        $data['status'] = 0;
+        EncoderTask::where('status', 19)->update($data);
+
     }
 
     //=========================================================================================================
