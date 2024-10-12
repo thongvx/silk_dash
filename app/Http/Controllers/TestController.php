4d56f0497f7d03b3d8a5df8d74e96de45eca764c
nnphuong <ngocphuongsv1@gmail.com>
2024-10-12 22:27:41 +0700

    phuong

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,19 +10,17 @@ class TestController extends Controller
     function test(){
         $arrStream = 'ss02-ss01';
         $explodeStream = explode('-', $arrStream);
+        $selectStream = null;
         foreach ($explodeStream as $svStream){
             $svStream = 'sv_streams:' . $svStream;
             $svStreamInfo = Redis::hgetall($svStream);
             if($svStreamInfo){
                 if ($svStreamInfo['out_speed'] < 900 && $svStreamInfo['active'] == 1) {
-                    return $svStreamInfo['domain'];
+                    $selectStream = $svStreamInfo['domain'];
                 }
             }
-            else
-            {
-                return null;
-            }
-        }
 
+        }
+        echo $selectStream;
     }
 }
