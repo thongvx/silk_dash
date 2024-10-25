58a98d8d84bae018204c58c88f04e66dd0069d4d
nnphuong <ngocphuongsv1@gmail.com>
2024-10-25 16:37:00 +0700

    phuong

--- app/Http/Controllers/admin/EncoderController.php
+++ app/Http/Controllers/admin/EncoderController.php
@@ -89,12 +89,10 @@ class EncoderController
     {
         $data = EncoderTask::where('status', 0)->where('quality', 720)->orderBy('priority', 'desc')->first();
         if($data){
-            //$dataUpdate['status'] = 1;
-            //$dataUpdate['sv_encoder'] = $name;
-//            $dataUpdate['start_encoder'] = now();
-//            EncoderTask::where('slug', $data->slug)->update($dataUpdate);
-//            $data->quality480 =
-//            $data->count = $count;
+            $dataUpdate['status'] = 1;
+            $dataUpdate['sv_encoder'] = $name;
+            $dataUpdate['start_encoder'] = now();
+            EncoderTask::where('slug', $data->slug)->update($dataUpdate);
             $quality480 = EncoderTask::where('slug', $data->slug)->where('quality', 480)->first();
             $quality1080 = EncoderTask::where('slug', $data->slug)->where('quality', 1080)->first();
 
